f1f6c3fde3f3bea3359091ab8f41cb71e9c531a0
henrysmitt <rickardsmitt@gmail.com>
2021-10-12 16:58:55 +0200

    pussel fungerar. Language placerar ut sina symboler

--- PROJ/Assets/Scripts/Puzzle/LanguagePuzzle.cs
+++ PROJ/Assets/Scripts/Puzzle/LanguagePuzzle.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,27 @@ using UnityEngine;
 public class LanguagePuzzle : Puzzle
 {
     //Place the symbols at the puzzle locations
+    private List<Transform> symbolPositions = new List<Transform>();
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlaceSymbols();
+            solution = "";
+            Translate(puzzleObjects);
+        }
+    }
+
+    private void PlaceSymbols()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            symbolPositions.Add(transform.GetChild(i));
+        }
+
+        for(int i = 0; i < puzzleObjects.Count; i++)
+        {
+            puzzleObjects[i].transform.position = symbolPositions[i].position;
+        }
+    }
 }
