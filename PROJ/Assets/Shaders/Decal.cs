32afef3665bfa73f268214c9e277bece8e931a1b
Pol Lozano Llorens <llop2000@gmail.com>
2021-10-13 13:54:13 +0200

    Added Decal shader

---
+++ PROJ/Assets/Shaders/Decal.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEditor;
+
+public class Decal : MonoBehaviour
+{
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        //For testing only
+        Handles.color = Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(transform.position, new Vector3(0, transform.localScale.y, transform.localScale.x));
+        Handles.DrawLine(transform.position, transform.position + transform.forward, 1);
+        Handles.ConeHandleCap(0, transform.position + transform.forward, Quaternion.LookRotation(transform.forward, Vector3.up), .2f, EventType.Repaint);
+    }
+#endif
+}
