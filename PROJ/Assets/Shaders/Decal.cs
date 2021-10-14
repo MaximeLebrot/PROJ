0df3544f4ae64688666aba4a49c94f9263d2b83f
electfall70 <llop2000@gmail.com>
2021-10-14 17:55:44 +0200

    terrain testing

--- PROJ/Assets/Shaders/Decal.cs
+++ PROJ/Assets/Shaders/Decal.cs
@@ -8,7 +8,8 @@ public class Decal : MonoBehaviour
     {
         //For testing only
         Handles.color = Gizmos.color = Color.white;
-        Gizmos.DrawWireCube(transform.position, new Vector3(0, transform.localScale.y, transform.localScale.x));
+        Gizmos.DrawWireCube(transform.position + transform.forward * transform.localScale.z / 2, new Vector3(0, transform.localScale.y, transform.localScale.x));
+        Gizmos.DrawWireCube(transform.position - transform.forward * transform.localScale.z / 2, new Vector3(0, transform.localScale.y, transform.localScale.x));
         Handles.DrawLine(transform.position, transform.position + transform.forward, 1);
         Handles.ConeHandleCap(0, transform.position + transform.forward, Quaternion.LookRotation(transform.forward, Vector3.up), .2f, EventType.Repaint);
     }
