75a4a32a3d6fe090be3723349945f9989eca94e3
deadlimit <47717668+deadlimit@users.noreply.github.com>
2021-10-19 20:51:39 +0200

    Smooth camera

--- PROJ/Assets/Scripts/DynamicCamera/DynamicCamera.cs
+++ PROJ/Assets/Scripts/DynamicCamera/DynamicCamera.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace DynamicCamera {
@@ -8,19 +9,17 @@ namespace DynamicCamera {
         [SerializeField] private LayerMask collisionMask;
 
         [SerializeField] private Vector3 offset;
-        [SerializeField] private float movementSpeed;
-        [SerializeField] private float rotationSpeed;
+        [SerializeField] [Range(0, 10)] private float followSpeed;
+        [SerializeField] [Range(0, 20)] private float rotationSpeed;
         [SerializeField] private float mouseSensitivity;
-       
+        
         private Vector2 input;
-        
-        private const string XRotationInputName = "Mouse Y";
-        private const string YRotationInputName = "Mouse X";
-        private new SphereCollider collider;
-        
+
+        private InputMaster inputMaster;
+
         private void Awake() {
-            Cursor.lockState = CursorLockMode.Locked;
-            collider = GetComponent<SphereCollider>();
+            inputMaster = new InputMaster();
+            inputMaster.Enable();
         }
         
         private void LateUpdate() {
@@ -30,31 +29,28 @@ namespace DynamicCamera {
         }
         
         private void GetInput() {
-            input.x -= Input.GetAxis(XRotationInputName) * mouseSensitivity;
-            input.y += Input.GetAxis(YRotationInputName) * mouseSensitivity;
 
-            input.x = Mathf.Clamp(input.x, -80, 80);
+            input.x -= inputMaster.Player.MoveCamera.ReadValue<Vector2>().y * mouseSensitivity;
+            input.y += inputMaster.Player.MoveCamera.ReadValue<Vector2>().x * mouseSensitivity;
+
+            input.x = Mathf.Clamp(input.x, -40, 60);
         }
         
         private void RotateCamera() {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(input.x, input.y, 0), rotationSpeed * Time.time);
+            target.localRotation = Quaternion.Lerp(target.localRotation, Quaternion.Euler(input.x, input.y, 0), rotationSpeed * Time.deltaTime);
+            transform.rotation = target.localRotation;
         }
 
         private void MoveCamera() {
 
             Vector3 collisionOffset = transform.rotation * offset;
             
-            if (Physics.SphereCast(target.position, collider.radius, collisionOffset.normalized, out RaycastHit hitInfo, collisionOffset.magnitude, collisionMask)) {
+            transform.position = Vector3.Slerp(transform.position, target.position + collisionOffset, followSpeed * Time.deltaTime);    
 
-                Vector3 projectOnPlane = Vector3.ProjectOnPlane(collisionOffset, hitInfo.normal);
+        }
 
-                collisionOffset = projectOnPlane;
-
-            }
-
-            Debug.DrawLine(target.position, target.position + collisionOffset, Color.red);
-            transform.position = Vector3.Slerp(transform.position, target.position + collisionOffset, movementSpeed * Time.deltaTime);    
-
+        private void OnApplicationFocus(bool hasFocus) {
+            Cursor.lockState = hasFocus ? CursorLockMode.Locked : CursorLockMode.None;
         }
     }
     
