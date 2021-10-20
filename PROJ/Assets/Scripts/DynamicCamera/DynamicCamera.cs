0bdafda23d81e889dba6c3de5fe69c389978031b
deadlimit <47717668+deadlimit@users.noreply.github.com>
2021-10-20 13:11:35 +0200

    Mer variabler i kameran

--- PROJ/Assets/Scripts/DynamicCamera/DynamicCamera.cs
+++ PROJ/Assets/Scripts/DynamicCamera/DynamicCamera.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace DynamicCamera {
     
@@ -10,27 +14,30 @@ namespace DynamicCamera {
         private Transform followTarget; 
         
         [SerializeField] private Transform eyeTarget;
-        
         [SerializeField] private LayerMask layerMask;
-
         [SerializeField] private Vector3 offset;
         [SerializeField] private Vector2 xClamp;
         [SerializeField] [Range(0, 20)] private float rotationSpeed;
+
         [SerializeField] private float mouseSensitivity;
         
         [SerializeField]
         [Tooltip("The lower the value the faster the camera moves")] 
         private float travelTime;
-
         private Vector2 input;
         private Vector3 velocity;
+        private Vector3 followTargetOriginalPosition;
+        
         
         private InputMaster inputMaster;
-
+        [SerializeField] [Range(0, 1)] private float minSlopeHeight;
+        [SerializeField] [Range(0, 1)] private float maxSlopeHeight;
+        [SerializeField] [Range(0, 10)] private float slopeAdjustmentSpeed;
 
         private void Awake() {
             inputMaster = new InputMaster();
             inputMaster.Enable();
+            followTargetOriginalPosition = followTarget.localPosition;
         }
         
         private void LateUpdate() {
@@ -39,7 +46,6 @@ namespace DynamicCamera {
             MoveCamera();
             
             CheckForTerrainHeight();
-            
         }
         
         private void GetInput() {
@@ -60,18 +66,21 @@ namespace DynamicCamera {
             Vector3 collisionOffset = transform.rotation * offset;
             
             transform.position = Vector3.SmoothDamp(transform.position, followTarget.position + collisionOffset, ref velocity, travelTime, 100, Time.deltaTime);
-
+            
         }
-
+        
         private void CheckForTerrainHeight() {
             Physics.Raycast(eyeTarget.position, eyeTarget.forward - eyeTarget.up, out var hit, 10,  layerMask);
+
+            float dot = 1 - Vector3.Dot(eyeTarget.up, hit.normal);
             
-            Debug.DrawRay(eyeTarget.position, eyeTarget.forward - eyeTarget.up, Color.red);
+            Vector3 targetHeight = dot <= maxSlopeHeight && dot >= minSlopeHeight ? Vector3.up : followTargetOriginalPosition;
+
+            followTarget.localPosition = Vector3.Lerp(followTarget.localPosition, targetHeight, slopeAdjustmentSpeed * dot * Time.deltaTime);
         }
 
-        private void OnApplicationFocus(bool hasFocus) {
-            Cursor.lockState = hasFocus ? CursorLockMode.Locked : CursorLockMode.None;
-        }
+        private void OnApplicationFocus(bool hasFocus) => Cursor.lockState = hasFocus ? CursorLockMode.Locked : CursorLockMode.None;
+        
     }
     
     
