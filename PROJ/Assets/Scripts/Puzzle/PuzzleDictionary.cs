15ad9df2265f0122283430643e7d13d5d230706d
henrysmitt <rickardsmitt@gmail.com>
2021-10-26 14:49:20 +0200

    puzzledictionary

---
+++ PROJ/Assets/Scripts/Puzzle/PuzzleDictionary.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleDictionary
+{
+
+    private Dictionary<int, PuzzleInstance> allPuzzles = new Dictionary<int, PuzzleInstance>(); /*puzzleID & puzzle*/
+
+
+
+}
