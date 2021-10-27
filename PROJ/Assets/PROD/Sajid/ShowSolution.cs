1d7b32ac1173ff1be412c3f3d5f2469d5e2600f4
Sajid <81232634+SajidMasoud@users.noreply.github.com>
2021-10-27 18:49:06 +0200

    fix

--- PROJ/Assets/PROD/Sajid/ShowSolution.cs
+++ PROJ/Assets/PROD/Sajid/ShowSolution.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class ShowSolution : MonoBehaviour
 {
-   // private InputMaster inputMaster;
+    private InputMaster inputMaster;
     private Animator anim;
+    public GameObject[] objects;
+    private int i;
     void Awake()
     {
-        anim = GetComponent<Animator>();
-    //    inputMaster = new InputMaster();
+       anim = GetComponent<Animator>();
+       inputMaster = new InputMaster();
     }
-    /*
+    
     private void OnEnable()
     {
         inputMaster.Enable();
@@ -20,20 +22,28 @@ public class ShowSolution : MonoBehaviour
     {
         inputMaster.Disable();
     }
-    */
+    
     private void PlayAnimation()
     {
         anim.SetBool("Solved", true);
 
     }
-    /*
+    
     // Update is called once per frame
     void Update()
     {
         if (inputMaster.PuzzleDEBUGGER.ShowSolution.triggered)
         {
-            anim.SetBool("Solved", true);
+            if (i < 3)
+            {
+                Debug.Log(i);
+
+                objects[i].SetActive(true);
+                i = i + 1;
+
+            }
         }
     }
-    */
+
+   
 }
