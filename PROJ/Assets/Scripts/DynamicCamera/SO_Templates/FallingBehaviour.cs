ad46129a8565a8db60f497acab9fdf60d5d41f86
deadlimit <47717668+deadlimit@users.noreply.github.com>
2021-10-27 19:30:59 +0200

    Glide fungerar, kan inte svänga karaktären dock

--- PROJ/Assets/Scripts/DynamicCamera/SO_Templates/FallingBehaviour.cs
+++ PROJ/Assets/Scripts/DynamicCamera/SO_Templates/FallingBehaviour.cs
@@ -1,9 +1,45 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Camera/Camera Behaviours/Falling Behaviour", fileName = "Falling Behaviour")]
 public class FallingBehaviour : OffsetCameraBehaviour {
+
+    [SerializeField] private float rotationSpeed;
+
+    private Quaternion rot;
     
     public override void Behave() {
         transform.position = Vector3.SmoothDamp(transform.position, followTarget.position + offset, ref velocity, followSpeed, 300, Time.deltaTime);
+        
+        Quaternion direction = quaternion.LookRotation(followTarget.position - transform.position, Vector3.up);
+        
+        transform.rotation = SmoothDamp(transform.rotation, direction, ref rot, rotationSpeed * Time.deltaTime);
+    }
+    
+    public Quaternion SmoothDamp(Quaternion rot, Quaternion target, ref Quaternion deriv, float time) {
+        if (Time.deltaTime < Mathf.Epsilon) return rot;
+        // account for double-cover
+        var Dot = Quaternion.Dot(rot, target);
+        var Multi = Dot > 0f ? 1f : -1f;
+        target.x *= Multi;
+        target.y *= Multi;
+        target.z *= Multi;
+        target.w *= Multi;
+        // smooth damp (nlerp approx)
+        var Result = new Vector4(
+            Mathf.SmoothDamp(rot.x, target.x, ref deriv.x, time),
+            Mathf.SmoothDamp(rot.y, target.y, ref deriv.y, time),
+            Mathf.SmoothDamp(rot.z, target.z, ref deriv.z, time),
+            Mathf.SmoothDamp(rot.w, target.w, ref deriv.w, time)
+        ).normalized;
+		
+        // ensure deriv is tangent
+        var derivError = Vector4.Project(new Vector4(deriv.x, deriv.y, deriv.z, deriv.w), Result);
+        deriv.x -= derivError.x;
+        deriv.y -= derivError.y;
+        deriv.z -= derivError.z;
+        deriv.w -= derivError.w;		
+		
+        return new Quaternion(Result.x, Result.y, Result.z, Result.w);
     }
 }
