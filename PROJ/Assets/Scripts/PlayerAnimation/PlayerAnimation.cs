b6664a60ac03d8ffc1918e19764881831cacd551
Sajid <81232634+SajidMasoud@users.noreply.github.com>
2021-10-27 12:26:50 +0200

    Fixat colliders på jointar osv för cloth physics och ett enkelt blend tree

---
+++ PROJ/Assets/Scripts/PlayerAnimation/PlayerAnimation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    //references
+    [SerializeField] private ControllerInputReference inputReference;
+    private Animator anim; 
+    
+    private Vector2 input;
+    private int x, y;
+    private void Awake()
+    {
+        x = Animator.StringToHash("speed");
+        y = Animator.StringToHash("direction");
+        anim = GetComponent<Animator>(); 
+    }
+
+    private void Update()
+    {
+        inputReference.InputMaster.Movement.ReadValue<Vector2>();
+        
+        anim.SetFloat(x, input.x);
+        anim.SetFloat(y, input.y);
+    }
+}
