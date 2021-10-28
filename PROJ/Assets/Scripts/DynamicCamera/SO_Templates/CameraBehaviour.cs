d88b399fc862a272d6ba68b96f8801dab3275d31
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2021-10-28 23:36:27 +0200

    3C_Camera_Dirty

--- PROJ/Assets/Scripts/DynamicCamera/SO_Templates/CameraBehaviour.cs
+++ PROJ/Assets/Scripts/DynamicCamera/SO_Templates/CameraBehaviour.cs
@@ -7,14 +7,30 @@ public abstract class CameraBehaviour : ScriptableObject {
     protected Transform followTarget;
     protected Transform transform;
     protected Vector3 velocity;
+    [SerializeField]protected DynamicCamera.DynamicCamera thisCamera;
 
     public virtual void Initialize(Transform objectTransform, Transform target) {
         transform = objectTransform;
         followTarget = target;
+        followTarget.localRotation = Quaternion.Euler(Vector3.zero);
+
     }
 
     public virtual void Behave() {}
 
     public virtual async Task BehaveAsync() => await Task.Yield();
-    
+
+
+
+
+    public Vector2 input;
+    public Vector2 GetInput()
+    {
+        return input;
+    }
+    public virtual void SetInput(Vector2 input)
+    {
+        this.input = input;
+    }
+
 }
