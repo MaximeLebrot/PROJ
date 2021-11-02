1b07cfe2a7d7a2dd7a62b1535dff732e2c8d83d1
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2021-11-02 14:27:53 +0100

    PC_Camera change

--- PROJ/Assets/Utilities/Input/InputMaster.cs
+++ PROJ/Assets/Utilities/Input/InputMaster.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Utilities/Input/InputMaster.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Utilities/Input/InputMaster.inputactions'
 
 using System;
 using System.Collections;
