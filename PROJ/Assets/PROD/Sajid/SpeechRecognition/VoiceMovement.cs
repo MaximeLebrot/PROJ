97ba203feff8a2a7d18abed942789812293cd69c
Sajid <81232634+SajidMasoud@users.noreply.github.com>
2021-11-11 14:23:02 +0100

    Fixat min prod

--- PROJ/Assets/PROD/Sajid/SpeechRecognition/VoiceMovement.cs
+++ PROJ/Assets/PROD/Sajid/SpeechRecognition/VoiceMovement.cs
@@ -4,24 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
+using UnityEngine.UI;
 
 
 public class VoiceMovement : MonoBehaviour
 {
     private KeywordRecognizer keywordRecognizer;
     private Dictionary<string, Action> actions = new Dictionary<string, Action>();
-    [SerializeField] private GameObject cube;
+   // [SerializeField] private GameObject Text;
+    public Text direction;
+    public Animator animator;
+
     private void Start()
     {
         actions.Add("forward", Forward);
-        actions.Add("up", Up);
-        actions.Add("down", Down);
+       // actions.Add("up", Up);
+       // actions.Add("down", Down);
         actions.Add("back", Back);
         actions.Add("right", Right);
         actions.Add("left", Left);
         actions.Add("rotate", Rotate);
-        cube.transform.Translate(0, 0, 0);
-
+        direction.text = "";
         keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
         keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
         keywordRecognizer.Start();
@@ -35,35 +38,35 @@ public class VoiceMovement : MonoBehaviour
     private void Forward()
     {
         transform.Translate(1, 0, 0);
+        direction.text = "Forward";
+        direction.GetComponent<Animator>().SetTrigger("active");
     }
 
     private void Back()
     {
         transform.Translate(-1, 0, 0);
+        direction.text = "Back";
+        direction.GetComponent<Animator>().SetTrigger("active");
     }
-
-    private void Up()
-    {
-        transform.Translate(0, 1, 0);
-    }
-
-    private void Down()
-    {
-        transform.Translate(0, -1, 0);
-    }
-
     private void Left()
     {
         transform.Translate(0, 0, 1);
+        direction.text = "Left";
+        direction.GetComponent<Animator>().SetTrigger("active");
     }
 
     private void Right()
     {
         transform.Translate(0, 0, -1);
+        direction.text = "Right";
+        direction.GetComponent<Animator>().SetTrigger("active");
     }
 
     private void Rotate()
     {
         transform.Rotate(0, 90, 0);
+        direction.text = "Rotate";
+        direction.GetComponent<Animator>().SetTrigger("active");
     }
+
 }
