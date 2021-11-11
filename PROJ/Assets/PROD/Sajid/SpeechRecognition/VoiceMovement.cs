ba8d235f3b815cd409b90484a659a2d7af2b470d
Sajid <81232634+SajidMasoud@users.noreply.github.com>
2021-11-11 15:53:08 +0100

    Push

--- PROJ/Assets/PROD/Sajid/SpeechRecognition/VoiceMovement.cs
+++ PROJ/Assets/PROD/Sajid/SpeechRecognition/VoiceMovement.cs
@@ -35,7 +35,7 @@ public class VoiceMovement : MonoBehaviour
         actions.Add("rotate twohundred and seventy", Rotate270);
         actions.Add("rotate two seventy", Rotate270);
 
-        actions.Add("Mums", David);
+        actions.Add("Bun", David);
         actions.Add("Dance", Dance);
         actions.Add("Kill", Die);
 
