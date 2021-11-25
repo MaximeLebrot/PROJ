3a0b79cd0fa795fbf0114b042f7b1b96d55c0b53
henrysmitt <rickardsmitt@gmail.com>
2021-11-25 21:17:12 +0100

    Revert "Revert "Revert "Merge branch 'main' into Peer-Review_26Nov"""
    This reverts commit c9b3b5ae4fd1bd4edcd4082eea6c437d0ec0eba9.

--- PROJ/Assets/Scripts/Puzzle/Puzzle.cs
+++ PROJ/Assets/Scripts/Puzzle/Puzzle.cs
@@ -1,6 +1,7 @@
+using System.Collections;
 using System.Collections.Generic;
-using System.Text;
 using UnityEngine;
+using System.Text;
 
 public class Puzzle : MonoBehaviour
 {
@@ -10,33 +11,34 @@ public class Puzzle : MonoBehaviour
     [SerializeField] private List<PuzzleInstance> puzzleInstances = new List<PuzzleInstance>();
     [SerializeField] private string playerInput = "";
     [SerializeField] protected string solution;
-    [SerializeField] Transform symbolPos;
-    
     protected PuzzleInstance currentPuzzleInstance;
+
     protected PuzzleTranslator translator = new PuzzleTranslator();
-    protected List<string> translations;
 
-    public PuzzleGrid grid;
-
-    private SymbolPlacer symbolPlacer;
- 
-    //should NOT be public, but ModularHintSystem currently relies on this List
-    public List<PuzzleObject> placedSymbols = new List<PuzzleObject>();
-
-    //track progress
+    private InputMaster inputMaster;
     private PuzzleCounter puzzleCounter;
+    protected PuzzleGrid grid;
+
+    //Draw symbols
+    [SerializeField] public List<PuzzleObject> instantiatedSymbols = new List<PuzzleObject>();
+    [SerializeField] Transform symbolPos;
+    [SerializeField] int symbolOffset;
+
+
+    //FKIN TECH LEEEEEED
     private int numOfPuzzles;
     private int currentPuzzleNum = 0;
+    private int numOfFinishedPuzzles = 0;
 
     private Transform player;
     private PuzzleParticles particles;
+    private List<string> translations;
 
     public float NextPuzzleTimer { get; } = 2.5f;
     public void SetPlayer(Transform t) { player = t; grid.Player = player; }
 
     void Awake()
     {
-        symbolPlacer = GetComponent<SymbolPlacer>();
         puzzleCounter = GetComponentInChildren<PuzzleCounter>();
         particles = GetComponentInChildren<PuzzleParticles>();
         if (puzzleInstances.Count > 0)
@@ -54,6 +56,11 @@ public class Puzzle : MonoBehaviour
         }
         else
             Debug.LogWarning("NO PUZZLE INSTANCES IN PUZZLE");        
+    }
+    private void Start()
+    {
+        inputMaster = new InputMaster();
+        inputMaster.Enable();
     }
 
     public void Load()
@@ -79,13 +86,12 @@ public class Puzzle : MonoBehaviour
     private void OnEnable()
     {
         EventHandler<ExitPuzzleEvent>.RegisterListener(ExitPuzzle);
-        EventHandler<ResetPuzzleEvent>.RegisterListener(ResetPuzzle);
         EventHandler<StartPuzzleEvent>.RegisterListener(StartPuzzle);
     }
     private void OnDisable()
     {
+        inputMaster.Disable();
         EventHandler<ExitPuzzleEvent>.UnregisterListener(ExitPuzzle);
-        EventHandler<ResetPuzzleEvent>.UnregisterListener(ResetPuzzle);
         EventHandler<StartPuzzleEvent>.UnregisterListener(StartPuzzle);
     }
     private void SetupPuzzleInstances()
@@ -98,13 +104,13 @@ public class Puzzle : MonoBehaviour
     }
 
     public virtual void InitiatePuzzle()
-    {       
+    {
+        
+        
         EventHandler<LoadPuzzleEvent>.FireEvent(new LoadPuzzleEvent(new PuzzleInfo(GetPuzzleID())));
         
         GetComponentInChildren<PuzzleStarter>().ResetStarter();
         //grid.ResetGrid();
-
-        currentPuzzleInstance.SetUpHazards();
 
         if (currentPuzzleInstance.HasRestrictions())
             grid.SetRestrictions(currentPuzzleInstance.GetRestrictions());
@@ -112,15 +118,13 @@ public class Puzzle : MonoBehaviour
         PlaceSymbols();
         solution = Translate();
         translations = translator.GetTranslations();
+
     }
     protected virtual void NextPuzzle()
     {
-        symbolPlacer.UnloadSymbols();
-        currentPuzzleInstance.DestroyHazards();
-        ResetClearanceVariables();
-
-
-        if (particles != null)
+        UnloadSymbols();
+        
+        if(particles != null)
             particles.Play();
 
         puzzleCounter.MarkedAsSolved(currentPuzzleNum);
@@ -128,7 +132,6 @@ public class Puzzle : MonoBehaviour
 
         if(currentPuzzleNum >= puzzleInstances.Count)
         {
-            symbolPlacer.UnloadSymbols();
             EventHandler<ActivatorEvent>.FireEvent(new ActivatorEvent(new PuzzleInfo(masterPuzzleID)));
             puzzleCounter.DeleteMarkers();
             CompletePuzzle();
@@ -164,13 +167,14 @@ public class Puzzle : MonoBehaviour
 
     private string Translate()
     {
-        if (placedSymbols.Count > 0)
-            return translator.CalculateSolution(placedSymbols);
+        if (instantiatedSymbols.Count > 0)
+            return translator.CalculateSolution(instantiatedSymbols);
         else
         {
             Debug.LogWarning("SOLUTION EMPTY, NO INSTANTIATED SYMBOLS");
             return null;
-        }         
+        }
+            
     }
     public virtual bool EvaluateSolution()
     {
@@ -189,17 +193,26 @@ public class Puzzle : MonoBehaviour
         return false;
     }
 
+    private void UnloadSymbols()
+    {
+        foreach(PuzzleObject po in instantiatedSymbols)
+        {
+            po.Unload();
+        }
+        instantiatedSymbols.Clear();
+    }
 
     private void OnTriggerExit(Collider other)
     {
-        PuzzleInfo info = new PuzzleInfo(currentPuzzleInstance.GetPuzzleID());
-        EventHandler<ExitPuzzleEvent>.FireEvent(new ExitPuzzleEvent(info, false));
+
+        //PuzzleInfo info = new PuzzleInfo(currentPuzzleInstance.GetPuzzleID());
+        //EventHandler<ExitPuzzleEvent>.FireEvent(new ExitPuzzleEvent(info, false));
+        grid.ResetGrid();
         GetComponentInChildren<PuzzleStarter>().ResetStarter();
 
     }
 
-
-
+    
     public void StartPuzzle(StartPuzzleEvent eve)
     {
         //Maybe this is dumb, ID comes from PuzzleInstance, but should technically be able to identify itself like this
@@ -220,63 +233,89 @@ public class Puzzle : MonoBehaviour
     public int GetPuzzleID() { return currentPuzzleInstance.GetPuzzleID(); }
 
 
-    private void ResetClearanceVariables()
-    {
-        solutionOffset = 0;
-        translationIndex = 0;
-        clearedSymbols.Clear();
-    }
+
 
     private int solutionOffset = 0;
     private int translationIndex = 0;
-    List<bool> clearedSymbols = new List<bool>();
 
-    public virtual void CheckIfClearedSymbol(string currentSolution) //currentSolution = what the player has drawn
+    public void CheckIfClearedSymbol(string currentSolution)
     {
+
+        
+
+        bool symbolComplete = false;
+        int solutionIndex = 0;
+        List<bool> clearedSymbols = new List<bool>();
+        Debug.Log(clearedSymbols.Count + " BOOLS");
+        Debug.Log(translations.Count + " Translations");
+
+
+        foreach(string s in translations)
+        {
+            Debug.Log(s);
+        }
 
 
         //goes through each index of strings in translations
-        for (int i = translationIndex; i < translations.Count; i++)
+        for (int i = 0; i < translations.Count; i++)
         {
-            if(IsEqualRange(solutionOffset, translations[i].Length, currentSolution))
+
+            //If the bool before this one is false, this one is false
+            if (i - 1 > 0 && clearedSymbols[i - 1] == false)
             {
-                translationIndex++;
-                solutionOffset += translations[i].Length;
-                clearedSymbols.Add(true);
+                clearedSymbols[i] = false;
+                continue;
             }
+
+
+
+            //goes through the string at [i] and checks the chars compared to currentSolution
+            for (int j = 0; j < translations[i].Length && solutionIndex + solutionOffset < currentSolution.Length; j++)
+            {
+                Debug.Log("i = " + i + " j = " + j);
+                if (currentSolution[solutionIndex + solutionOffset] == translations[i][j])
+                {
+                    if(solutionIndex + solutionOffset + 1 == translations[i].Length)
+                    {
+                        clearedSymbols.Add(true); //?????????????????
+                        symbolComplete = true;
+                        solutionOffset += solutionIndex + 1;
+                        solutionIndex = 0;
+                        //translationIndex++;
+                        break;
+                    }
+
+                    solutionIndex++;
+                    continue;
+                }  
+                else
+                {
+                    
+                    break;
+                }
+            }
+
+            if(symbolComplete == false)
+            {
+                if (currentSolution.Length - solutionOffset < translations[i].Length)
+                    clearedSymbols.Add(false);
+            }
+            
+
+            //DETTA E FEL VI HAR REDAN LAGT TILL? VI BORDE VETA DETTA I LOOPEN
+            
+
+        }
+
+        for(int i = 0; i < instantiatedSymbols.Count; i++)
+        {
+            if (clearedSymbols[i] == true)
+                instantiatedSymbols[i].TurnOn();
             else
-            {
-                break;
-            }
-
+                instantiatedSymbols[i].TurnOff();
         }
-
-        //Debug.Log(clearedSymbols.Count + " BOOLS");
-
-        for (int i = 0; i < clearedSymbols.Count; i++)
-        {
-            if(placedSymbols[i].Active == false)
-                placedSymbols[i].Activate();
-        }
-
      }
 
-    private bool IsEqualRange(int offset, int length, string currentSolution)
-    {
-        //Debug.Log(length);
-
-        if (offset + length > currentSolution.Length)
-        {
-            //Debug.Log("LENGTH IS WRONG");
-            return false;
-        }
-
-        Debug.Log("input: " + currentSolution.Substring(offset, length) + " translation: " + translations[translationIndex]);
-        //Debug.Log("equal = " + currentSolution.Substring(offset, length).Equals(translations[translationIndex]));
-
-        return currentSolution.Substring(offset, length).Equals(translations[translationIndex]);
-        
-    }
 
     public void ExitPuzzle(ExitPuzzleEvent eve)
     {
@@ -286,46 +325,99 @@ public class Puzzle : MonoBehaviour
             {
                 if(eve.success == false)
                 {
-                    symbolPlacer.UnloadSymbols();
                     grid.ResetGrid();
                 }
                     
             }
         }
+
     }
-    //To manage the number of times ResetPuzzle is subscribed to its event, quick fix dont judge pls
-    private bool registered = true;
-    private void ResetPuzzle(ResetPuzzleEvent eve)
+
+
+    #region Place Symbols
+    private void PlaceSymbols()
     {
-
-        EventHandler<ResetPuzzleEvent>.UnregisterListener(ResetPuzzle);
-        registered = false;
-        Debug.Log("Reset puzzle called");
-        if (eve.info.ID == currentPuzzleInstance.GetPuzzleID())
+        if (instantiatedSymbols.Count > 0)
         {
-            symbolPlacer.UnloadSymbols();
-            grid.ResetGrid();
+            UnloadSymbols();
+        }
 
+
+        //Is this the way we want to fetch the list??
+        foreach (SymbolModPair pair in currentPuzzleInstance.puzzleObjects)
+        {
+            GameObject instance = Instantiate(pair.symbol).gameObject;
+
+            PuzzleObject objectInstance = instance.GetComponent<PuzzleObject>();
+            instantiatedSymbols.Add(objectInstance);
+            objectInstance.transform.parent = symbolPos;
+            objectInstance.SetModifier(pair.modifier);
+
+        }
+
+        if (instantiatedSymbols.Count % 2 == 0)
+            EvenPlaceSymbols();
+        else
+            UnevenPlaceSymbols();
+    }
+
+    private void UnevenPlaceSymbols()
+    {
+        Quaternion emptyQ = new Quaternion(0, 0, 0, 0);
+        int mid = instantiatedSymbols.Count / 2;
+        instantiatedSymbols[mid].transform.localPosition = Vector3.zero;
+        instantiatedSymbols[mid].transform.localRotation = emptyQ;
+
+        for (int i = 1; i <= mid; i++)
+        {
+            Vector3 tempPos = Vector3.zero;
+            tempPos -= i * (symbolOffset * Vector3.right);
+            instantiatedSymbols[mid - i].transform.localPosition = tempPos;
+            instantiatedSymbols[mid - i].transform.rotation = symbolPos.rotation;
+            instantiatedSymbols[mid - i].transform.localPosition =
+                new Vector3(instantiatedSymbols[mid - i].transform.localPosition.x, 0, 0);
+
+
+
+            tempPos = Vector3.zero;
+            tempPos += i * (symbolOffset * Vector3.right);
+            instantiatedSymbols[mid + i].transform.localPosition = tempPos;
+            instantiatedSymbols[mid + i].transform.rotation = symbolPos.rotation;
+            instantiatedSymbols[mid + i].transform.localPosition =
+                new Vector3(instantiatedSymbols[mid + i].transform.localPosition.x, 0, 0);
         }
     }
 
-    public void RegisterToResetPuzzleEvent()
+    private void EvenPlaceSymbols()
     {
-        if (registered)
-            return;
 
-        Debug.Log("Registered listener to reset puzzle event");
-        EventHandler<ResetPuzzleEvent>.RegisterListener(ResetPuzzle);
-        registered = true;
-    }
-    
-    private void PlaceSymbols()
-    {
-        placedSymbols = symbolPlacer.PlaceSymbols(currentPuzzleInstance, symbolPos);
-        
-    }
 
-     
+        int midRight = instantiatedSymbols.Count / 2;
+        int midLeft = midRight - 1;
+
+        Vector3 midLeftPos = (Vector3.left * (symbolOffset / 2));
+        Vector3 midRightPos = (Vector3.right * (symbolOffset / 2));
+
+        instantiatedSymbols[midLeft].transform.localPosition = midLeftPos;
+        instantiatedSymbols[midLeft].transform.localRotation = new Quaternion(0, 0, 0, 0);
+
+        instantiatedSymbols[midRight].transform.localPosition = midRightPos;
+        instantiatedSymbols[midRight].transform.rotation = new Quaternion(0, 0, 0, 0);
+
+        for (int i = 1; i <= midLeft; i++)
+        {
+            Vector3 tempPos = midLeftPos;
+            tempPos -= i * (symbolOffset * Vector3.right);
+            instantiatedSymbols[midLeft - i].transform.localPosition = tempPos;
+            instantiatedSymbols[midLeft - i].transform.rotation = symbolPos.rotation;
+
+            tempPos = midRightPos;
+            tempPos += i * (symbolOffset * Vector3.right);
+            instantiatedSymbols[midRight + i].transform.localPosition = tempPos;
+            instantiatedSymbols[midRight + i].transform.rotation = symbolPos.rotation;
+        }
+    }
+    #endregion    
 }
 
 
