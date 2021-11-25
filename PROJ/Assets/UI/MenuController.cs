affbcb51a7d57a9bd82e5d4932772aad28d22cf6
deadlimit <47717668+deadlimit@users.noreply.github.com>
2021-11-25 14:24:29 +0100

    UI

---
+++ PROJ/Assets/UI/MenuController.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+    [SerializeField] private ControllerInputReference controllerInputReference;
+    private Animator animator;
+
+    private int onAnyKeyHash;
+    private int showMainMenu;
+    private int showSettingsMenu;
+    private int showGeneralHash;
+    private int showVideoHash;
+    private int showAudioHash;
+    private int showAccessibilityHash;
+
+    [SerializeField] private GraphicRaycaster raycaster;
+    
+    private Stack<int> depth = new Stack<int>();
+
+    private Dictionary<string, int> pageHashes = new Dictionary<string, int>();
+    
+    public delegate void ActivatePage(int ID);
+    public static event ActivatePage OnActivatePage;
+    
+    private void Awake() {
+        controllerInputReference.Initialize();
+        
+        animator = GetComponent<Animator>();
+
+        onAnyKeyHash = Animator.StringToHash("AnyKeyPressed");
+        showSettingsMenu = Animator.StringToHash("ShowSettingsMenu");
+        showGeneralHash = Animator.StringToHash("ShowGeneral");
+        showVideoHash = Animator.StringToHash("ShowVideo");    
+        showAudioHash = Animator.StringToHash("ShowAudio");    
+        showAccessibilityHash = Animator.StringToHash("ShowAccessibility");
+        
+        pageHashes.Add("SettingsMenu", showSettingsMenu);
+        pageHashes.Add("General", showGeneralHash);
+        pageHashes.Add("Video", showVideoHash);
+        pageHashes.Add("Audio", showAudioHash);
+        pageHashes.Add("Accessibility", showAccessibilityHash);
+        
+        controllerInputReference.InputMaster.Anykey.performed += OnAnyKeyPressed;
+        controllerInputReference.InputMaster.Menu.performed += GoBack;
+       
+    }
+    
+    private void OnAnyKeyPressed(InputAction.CallbackContext e) {
+        controllerInputReference.InputMaster.Anykey.performed -= OnAnyKeyPressed;
+        
+        animator.SetTrigger(onAnyKeyHash);
+    }
+    
+    public async void SwitchPage(string pageName) {
+        animator.SetBool(pageHashes[pageName], true);
+        
+        depth.Push(pageHashes[pageName]);
+    }
+
+    private void GoBack(InputAction.CallbackContext e) {
+
+        if (depth.Count < 1)
+            return;
+        
+        int currentLevel = depth.Pop();
+        
+        animator.SetBool(currentLevel, !animator.GetBool(currentLevel));
+        
+    }
+    
+    public void OnActivatePageEvent(string name) => OnActivatePage?.Invoke(name.GetHashCode());
+}
