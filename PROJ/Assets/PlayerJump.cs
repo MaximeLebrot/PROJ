dcc4463494059dc17c2148a33d01cebb40f5a636
Sajid <81232634+SajidMasoud@users.noreply.github.com>
2021-11-29 14:12:47 +0100

    Pillat på char jump animation

--- PROJ/Assets/PlayerJump.cs
+++ PROJ/Assets/PlayerJump.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class PlayerJump : MonoBehaviour
 {
     public Animator JumpAnimator;
-
+    MetaPlayerController MPC;
+   
     void OnTriggerEnter(Collider col)
     {
+
         if (col.CompareTag("Player"))
         {
             JumpAnimator.SetTrigger("First");
             Debug.Log("Its jumping");
+            MPC = col.GetComponent<MetaPlayerController>();
+            MPC.playerController3D.enabled = false;
+            MPC.physics.enabled = false;
+            MPC.transform.parent = this.transform;
         }
+    }
+
+    public void ActivateScripts()
+    {
+        GetComponent<SphereCollider>().enabled = false;
+        MPC.transform.parent = null;
+        MPC.playerController3D.enabled = true;
+        MPC.physics.enabled = true;
+        MPC.physics.velocity = Vector3.zero;
+        Debug.Log("Rickard");
     }
 }
 
