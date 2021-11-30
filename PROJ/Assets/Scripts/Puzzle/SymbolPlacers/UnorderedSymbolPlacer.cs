65b03106ccef7eb72223861f4a1368188d75cae8
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2021-11-30 11:00:40 +0100

    Meshes for symbol and modifier prefabs, tweaked unordered placer

--- PROJ/Assets/Scripts/Puzzle/SymbolPlacers/UnorderedSymbolPlacer.cs
+++ PROJ/Assets/Scripts/Puzzle/SymbolPlacers/UnorderedSymbolPlacer.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class UnorderedSymbolPlacer : SymbolPlacer
 {
-    [SerializeField] private List<Transform> symbolPositions = new List<Transform>();
     //randomizing starting offset between these 2 values, for visual variation
     [SerializeField]private Vector2 offsetMinMax = new Vector2(45, 90);
     private List<PuzzleObject> instantiatedSymbolsClone;
@@ -13,12 +12,21 @@ public class UnorderedSymbolPlacer : SymbolPlacer
     private float degreeOffset;
     private float startingOffset;
     private float panelSize = 2f;
+    private float panelPlacementDepthOffset = 0.1f;
 
     protected override void UnevenPlaceSymbols()
     {
         //Should probably only relate to the correct number of positions (if we have predefined acceptable positions for placement), uneven or even shouldnt matter.
+        CalculateSymbolPosition();
         EvenPlaceSymbols();
     }
+
+    private void CalculateSymbolPosition()
+    {
+        Physics.Raycast(transform.position + Vector3.up * 2, Vector3.forward, out RaycastHit panelHit);
+        Quaternion.Euler(panelHit.normal);
+    }
+
     protected override void EvenPlaceSymbols()
     {
         startingOffset = Random.Range(offsetMinMax.x, offsetMinMax.y);
@@ -41,16 +49,13 @@ public class UnorderedSymbolPlacer : SymbolPlacer
 
     private void PlaceSymbols(Vector3 newPos)
     {
-
         //Move and REmove instance from list
-
         PuzzleObject instance = instantiatedSymbolsClone[Random.Range(0, instantiatedSymbolsClone.Count)];       
 
         instance.transform.position = newPos;
-        instance.transform.rotation = new Quaternion(0,0,0,0);
-        instance.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+        instance.transform.rotation = symbolPos.rotation;
 
-        Vector3 pos = new Vector3(instance.transform.localPosition.x, instance.transform.localPosition.y, -2);
+        Vector3 pos = new Vector3(instance.transform.localPosition.x, instance.transform.localPosition.y, panelPlacementDepthOffset);
         instance.transform.localPosition = pos;
 
         instantiatedSymbolsClone.Remove(instance);
