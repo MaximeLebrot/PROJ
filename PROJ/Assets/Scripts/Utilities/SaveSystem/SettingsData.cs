40fa7368d194665470b85ad3346e9a950d030a1d
deadlimit <47717668+deadlimit@users.noreply.github.com>
2021-12-08 14:42:30 +0100

    YEA

--- PROJ/Assets/Scripts/Utilities/SaveSystem/SettingsData.cs
+++ PROJ/Assets/Scripts/Utilities/SaveSystem/SettingsData.cs
@@ -1,39 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System;
 
 [Serializable]
 public class SettingsData
 {
+    //General
+    public float mouseSensitivity;
+    public bool holdToSprint;
+    public bool pressToSprint;
+    
     //Audio
+    public float masterVolume;
     public float musicVolume;
     public float voiceVolume;
     public float soundEffectsVolume;
     public bool mute;
-
-    //Easy of Access
-    public int fontSize;
-    public float pointerSize;
-    public bool showDesktop; //What is this? 
-    public bool blindMode;
-    public bool highContrastMode;
+    public float ambience;
     
-
     //Display
     public float fieldOfView;
     public float brightness;
-    //public GraphicsQuality Quality;
     public bool fullscreen;
-    //private Resolution screenRes? 
-
-
-    //Puzzle Settings
-    public bool currentNodeMarker;
+    public Resolution screenResolution;
+    public string quality;
+    
+    //Accessibility
+    public int fontSize;
+    public bool blindMode;
+    public bool highContrastMode;
+    public bool dyslexiaFont;
+    public bool oneHandMode;
     public float nodeSize;
     public float lineSize;
-    public bool animatedNodes;
-    public bool animatedLines;
+    public bool currentNodeMarker;
+    public bool showClearedSymbols;
+    public bool easyPuzzleControls;
+    
 }
 
 [Serializable]
