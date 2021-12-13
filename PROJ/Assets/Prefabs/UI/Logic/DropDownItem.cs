ad379c091c868eb3cda5fba51fb544f0e4258472
henrysmitt <rickardsmitt@gmail.com>
2021-12-13 14:49:18 +0100

    joanthan till rick

--- PROJ/Assets/Prefabs/UI/Logic/DropDownItem.cs
+++ PROJ/Assets/Prefabs/UI/Logic/DropDownItem.cs
@@ -6,7 +6,9 @@ public class DropDownItem : UIMenuItem {
     [SerializeField] private TMP_Dropdown dropdownList;
     
     protected override void Initialize() {}
+    public override dynamic GetValue() {
+        return dropdownList.options[dropdownList.value].text;
+    }
 
-    public override dynamic GetValue() => dropdownList.options[dropdownList.value].text;
     public override void SetValue(dynamic value) => dropdownList.value = dropdownList.options.FindIndex(resolutionOption => resolutionOption.text.Equals(value.ToString()));
 }
