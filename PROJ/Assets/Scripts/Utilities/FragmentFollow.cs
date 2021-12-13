3369895c2be6702c792a6220f6e70ba22a95c64b
MaximeLebrot <maximelebrot@live.se>
2021-12-13 17:44:02 +0100

    fragment och tutorial

--- PROJ/Assets/Scripts/Utilities/FragmentFollow.cs
+++ PROJ/Assets/Scripts/Utilities/FragmentFollow.cs
@@ -2,24 +2,36 @@ using UnityEngine;
 
 public class FragmentFollow : MonoBehaviour
 {
-    public GameObject holder;
-    public GameObject player;
-    public GameObject fragment;
-    public GameObject fancyBall;
-    public float offset = 2f;
-    private Vector3 pos;
+    public Transform fragmentHolder;
+    public Transform fragmentOrb;
+    public float speed = 5f;
+    [SerializeField, Range(0.1f, 1f)] private float scale = 0.5f;
+    [SerializeField, Range(0.1f, 2f)] private float fragmentSize = 1f;
+
+    private Vector3 downScale;
+    private bool follow;
 
     private void Start()
     {
-        fancyBall.transform.parent = transform;
-        fancyBall.transform.localPosition = Vector3.zero;
+        scale /= 10;
+        downScale = new Vector3(scale, scale, scale);    
     }
-    private void FixedUpdate()
+
+    private void OnTriggerEnter(Collider other)
     {
-        pos = player.transform.position;
-        pos.y += offset;
-        holder.transform.position = pos;
-        //fancyBall.transform.position = transform.position;
+        if (other.CompareTag("Player"))
+            follow = true;
+    }
+
+    private void Update()
+    {
+        if (follow)
+        {
+            if (transform.localScale.x > fragmentSize)
+                transform.localScale -= downScale;
+            if (Vector3.Distance(fragmentOrb.position, fragmentHolder.position) > 0.1f)
+                fragmentOrb.position = Vector3.Lerp(fragmentOrb.position, fragmentHolder.position, Time.deltaTime * speed);
+        }
     }
 
 }
