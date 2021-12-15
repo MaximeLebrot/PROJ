8af6c65a0f60adc8f93c882ae451756d9f32a6a6
MaximeLebrot <maximelebrot@live.se>
2021-12-15 17:44:01 +0100

    OS

--- PROJ/Assets/PROD/Maxime/PROD2/Scripts/States/OSPuzzleState.cs
+++ PROJ/Assets/PROD/Maxime/PROD2/Scripts/States/OSPuzzleState.cs
@@ -1,14 +1,31 @@
+using System.Collections;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "PlayerStates/OneSwitchStates/OSPuzzleState")]
 public class OSPuzzleState : PlayerState
 {
-    private bool realignPlayer;
-    private Transform puzzleTransform;
-    private Vector3 offset = new Vector3(0, 1.1f, 0);
+    private OSPuzzle puzzle;
 
+    [Header("Variables"), SerializeField, Range(0.1f, 0.9f)] private float speed = 0.5f;
+    [SerializeField, Range(0.01f, 0.3f)] private float holdingButtonLimit = 0.2f;
+    [SerializeField] private float puzzleWalkDuration = 1f;
+
+    private float time = 1f, timer;
+    private int iterator = 2;
+    private float frameCounter;
+    private bool giveLostTime = true;
+    private bool pressingButton;
+    private bool movingPlayer;
+    private bool diagonalMove;
+    private bool realignPlayer;
+
+    private Transform puzzleTransform;
+
+    private Vector3 offset = new Vector3(0, 1.1f, 0);
+    private Vector3 currentWalkingDirection = new Vector3();
     private Vector2 playerXZ = new Vector2();
     private Vector2 puzzleXZ = new Vector2();
+
     public override void Initialize() => base.Initialize();
 
     public override void EnterState() => HandleStateEntry();
@@ -18,8 +35,104 @@ public class OSPuzzleState : PlayerState
     public override void RunUpdate()
     {
         HandleStartingAlignment();
+        HandlePuzzleMovement();
+    }
 
-        //When do we leave puzzle state, only controlled by EndPuzzleEvent? 
+    private void HandlePuzzleMovement()
+    {
+        if (player.inputReference.inputMaster.OneSwitch.OnlyButton.ReadValue<float>() != 0)
+        {
+            pressingButton = true;
+            frameCounter += Time.deltaTime;
+            if (frameCounter >= holdingButtonLimit)
+            {
+                if (giveLostTime)
+                    timer += frameCounter;
+                giveLostTime = false;
+                if (timer >= time)
+                {
+                    if (iterator >= puzzle.UINodes.Count)
+                        iterator = 0;
+                    foreach (OSPuzzleNode node in puzzle.UINodes)
+                        node.DeselectPuzzleNode();
+                    puzzle.UINodes[iterator].SelectPuzzleNode();
+                    iterator++;
+                    timer = 0;
+                }
+                else
+                    timer += Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (pressingButton)
+            {
+                if (frameCounter < holdingButtonLimit && !movingPlayer)
+                    MovePlayerTo(GetActiveButton());
+                frameCounter = 0;
+                pressingButton = false;
+                giveLostTime = true;
+            }
+        }
+        if (movingPlayer)
+            player.puzzleController.SetInput(currentWalkingDirection);
+    }
+
+    IEnumerator WalkToNode()
+    {
+        movingPlayer = true;
+        float walkDuration = puzzleWalkDuration;
+        if (diagonalMove)
+            walkDuration = Mathf.Sqrt(Mathf.Pow(puzzleWalkDuration, 2) * 2);
+        yield return new WaitForSeconds(walkDuration);
+        diagonalMove = false;
+        movingPlayer = false;
+    }
+
+    private int GetActiveButton()
+    {
+        OSPuzzleNode node = null;
+        foreach (OSPuzzleNode n in puzzle.UINodes)
+        {
+            if (n.GetSelected())
+                node = n;
+        }
+        return node.number;
+    }
+
+    private void MovePlayerTo(int numPDirection)
+    {
+        currentWalkingDirection = ConvertNodeNumberToVecDir(numPDirection);
+        puzzle.StartCoroutine(WalkToNode());
+        //Debug.Log(numPDirection + " = " + numPDirection + ", Dir: " + ConvertNodeNumberToDirection(numPDirection));
+    }
+
+    private Vector2 ConvertNodeNumberToVecDir(int number)
+    {
+        switch (number)
+        {
+            case 1:
+                diagonalMove = true;
+                return Vector2.up + Vector2.left;
+            case 2:
+                return Vector2.up;
+            case 3:
+                diagonalMove = true;
+                return Vector2.up + Vector2.right;
+            case 4:
+                return Vector2.right;
+            case 5:
+                diagonalMove = true;
+                return Vector2.down + Vector2.right;
+            case 6:
+                return Vector2.down;
+            case 7:
+                diagonalMove = true;
+                return Vector2.down + Vector2.left;
+            case 8:
+                return Vector2.left;
+        }
+        return Vector2.zero;
     }
 
     private void HandleStartingAlignment()
@@ -53,6 +166,8 @@ public class OSPuzzleState : PlayerState
     {
         base.EnterState();
         Debug.Log("Puzzle state");
+        time = 1f - speed;
+        timer = time;
         realignPlayer = true;
         player.physics.velocity = Vector3.zero;
         player.playerController3D.enabled = false;
@@ -64,7 +179,9 @@ public class OSPuzzleState : PlayerState
         puzzleTransform = eve.info.puzzlePos;
         puzzleXZ.x = puzzleTransform.position.x;
         puzzleXZ.y = puzzleTransform.position.z;
+        puzzle = eve.info.puzzlePos.gameObject.GetComponent<OSPuzzle>();
     }
+
     private void OnEnable() => EventHandler<StartPuzzleEvent>.RegisterListener(GetPuzzleInfo);
 
     private void OnDisable() => EventHandler<StartPuzzleEvent>.UnregisterListener(GetPuzzleInfo);
