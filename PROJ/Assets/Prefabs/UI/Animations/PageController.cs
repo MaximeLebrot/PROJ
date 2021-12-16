41070190ec6f6a25641b07c813e9f619d511c808
deadlimit <47717668+deadlimit@users.noreply.github.com>
2021-12-16 13:10:18 +0100

    Main menu

--- PROJ/Assets/Prefabs/UI/Animations/PageController.cs
+++ PROJ/Assets/Prefabs/UI/Animations/PageController.cs
@@ -68,6 +68,10 @@ public class PageController : MonoBehaviour {
     }
 
     public bool CanMoveUpOneLevel() {
+
+        if (subMenuDepth.Count < 1)
+            return false;
+        
         subMenuDepth.Pop();
 
         if (subMenuDepth.Count > 0) {
