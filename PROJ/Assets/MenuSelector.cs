5a47df035b5a612aa03f69a1203bff08a5d59d3d
deadlimit <47717668+deadlimit@users.noreply.github.com>
2022-01-05 16:10:40 +0100

    MainMenu

---
+++ PROJ/Assets/MenuSelector.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MenuSelector : MonoBehaviour {
+
+
+    [SerializeField] private RectTransform selectorRect;
+
+    private void Awake() {
+        MainMenuButton.OnHover += MoveSelector;
+    }
+    
+    private void MoveSelector(Transform target) {
+        selectorRect.parent = target.transform;
+        selectorRect.localPosition = Vector2.zero;
+    }
+
+    public void Quit() => Application.Quit();
+}
