f57a9f55dcd5d23ed8f84ab90e8ae63d3137b684
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2022-01-06 12:05:20 +0100

    Volume new menu settings

--- PROJ/Assets/Audio/VolumeController.cs
+++ PROJ/Assets/Audio/VolumeController.cs
@@ -16,12 +16,33 @@ public class VolumeController : MonoBehaviour
     {
         VcaController = FMODUnity.RuntimeManager.GetVCA("vca:/Master");
 
-        (GameMenuController.Instance.RequestOption<MasterVolume>() as MasterVolume).AddListener((value) => SetMasterVolume(value));
-        (GameMenuController.Instance.RequestOption<MusicVolume>() as MusicVolume).AddListener((value) => SetMusicVolume(value));
-        (GameMenuController.Instance.RequestOption<SFXVolume>() as SFXVolume).AddListener((value) => SetSFXVolume(value));
-        (GameMenuController.Instance.RequestOption<VoiceVolume>() as VoiceVolume).AddListener((value) => SetVoiceVolume(value));
+        (GameMenuController.Instance.RequestOption<MasterVolume>() as MasterVolume).AddListener(SetMasterVolume);
+        (GameMenuController.Instance.RequestOption<MusicVolume>() as MusicVolume).AddListener(SetMusicVolume);
+        (GameMenuController.Instance.RequestOption<SFXVolume>() as SFXVolume).AddListener(SetSFXVolume);
+        (GameMenuController.Instance.RequestOption<VoiceVolume>() as VoiceVolume).AddListener(SetVoiceVolume);
     }
 
+    private void SetMasterVolume(float val)
+    {
+        masterVolume = val;
+        VcaController.setVolume(masterVolume);
+    }
+    private void SetMusicVolume(float val)
+    {
+        musicVolume = val; 
+    }
+    private void SetSFXVolume(float val)
+    {
+       soundEffectsVolume = val;
+    }
+    private void SetVoiceVolume(float val)
+    {
+        voiceVolume = val;
+    }
+
+
+    #region Legacy listener
+    /*
     private void OnEnable()
     {
         EventHandler<SaveSettingsEvent>.RegisterListener(UpdateVolume);
@@ -41,22 +62,6 @@ public class VolumeController : MonoBehaviour
         voiceVolume = eve.settingsData.voiceVolume;
 
         VcaController.setVolume(masterVolume);
-    }
-    private void SetMasterVolume(float val)
-    {
-        masterVolume = val;
-        VcaController.setVolume(masterVolume);
-    }
-    private void SetMusicVolume(float val)
-    {
-        musicVolume = val; 
-    }
-    private void SetSFXVolume(float val)
-    {
-       soundEffectsVolume = val;
-    }
-    private void SetVoiceVolume(float val)
-    {
-        voiceVolume = val;
-    }
+    }*/
+    #endregion
 }
