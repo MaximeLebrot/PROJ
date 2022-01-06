bdb16171ec306005bdbe782aaed71cc7e96344af
MaximeLebrot <maximelebrot@live.se>
2022-01-06 20:13:35 +0100

    Update LogbookUpdater.cs

--- PROJ/Assets/PROD/Astrid/LogbookUpdater.cs
+++ PROJ/Assets/PROD/Astrid/LogbookUpdater.cs
@@ -4,20 +4,16 @@ using UnityEngine;
 public class LogbookUpdater : MonoBehaviour
 {
     [SerializeField] private int puzzleID;
+    [SerializeField] private Logbook book;
     [SerializeField] private int pagesAdded;
-    [SerializeField] private Logbook log;
 
     [Header("Options for adding right page"), SerializeField] private bool rightPageOnly;
     [SerializeField] private string[] rightSidePagesName;
-    [SerializeField] private Page[] rightSidePages;
-    private static Logbook book;
 
 
     private void Awake()
     {
-        if (log != null)
-            book = log;
-        //FindLogbook();
+        FindLogbook();
     }
 
     private void FindLogbook()
@@ -41,12 +37,9 @@ public class LogbookUpdater : MonoBehaviour
     {
         if (eve.info.ID == puzzleID)
         {
-            Debug.Log("Eve info ID: " + eve.info.ID);
-            FindLogbook();
+            //FindLogbook();
             if (rightPageOnly)
             {
-                //for (int i = 0; i < rightSidePages.Length; i++)
-                    //book.AddRightSide(rightSidePages[i]);
                 for (int i = 0; i < rightSidePagesName.Length; i++)
                     book.AddRightSide(rightSidePagesName[i]);
             }
