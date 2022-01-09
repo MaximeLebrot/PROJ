e9828b2ae180234a258f335c8394add5f20d9735
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2022-01-09 22:35:44 +0100

    SoundEvent UNREGISTER

--- PROJ/Assets/Scripts/Puzzle/PuzzleUtilities/PuzzleStarter.cs
+++ PROJ/Assets/Scripts/Puzzle/PuzzleUtilities/PuzzleStarter.cs
@@ -18,7 +18,7 @@ public class PuzzleStarter : MonoBehaviour
         if (active == false)
         {
             puzzle.GetComponent<SphereCollider>().enabled = true;
-            EventHandler<StartPuzzleEvent>.FireEvent(new StartPuzzleEvent(new PuzzleInfo(puzzle.GetPuzzleID(), GetComponentInParent<Puzzle>())));
+            EventHandler<StartPuzzleEvent>.FireEvent(new StartPuzzleEvent(new PuzzleInfo(puzzleID, puzzle)));
             puzzle.SetPlayer(other.transform);
             active = true;
         }
